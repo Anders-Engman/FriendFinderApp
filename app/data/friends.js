60afbc56a0c5e80ac88f0217d69a8ab379fef443
A-Engman <anders.p.engman@gmail.com>
2018-11-08 18:45:42 -0500

    First Additions

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with seed data
+var friendList = [
+    {
+      name: 'Anders Engman',
+      profilePic: 'https://i.imgur.com/ReG2e2e.gif',
+      scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4"
+      ]
+    }, {
+        name: 'Ian Kimble',
+        profilePic: 'https://i.imgur.com/NFFHjpG.gif',
+        scores: [
+            "3",
+            "2",
+            "4",
+            "1",
+            "5",
+            "4",
+            "5",
+            "4",
+            "3",
+            "5"
+        ]
+    }
+  ]
+  
+  module.exports = friendList;
